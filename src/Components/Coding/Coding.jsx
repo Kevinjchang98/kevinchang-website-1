7867a12feb15073eb445970183c3af8c2afbfef2
Kevin Chang <kevinjchang98@gmail.com>
2020-07-22 12:46:26 -0700

    add fade in

--- src/Components/Coding/Coding.jsx
+++ src/Components/Coding/Coding.jsx
@@ -2,35 +2,39 @@ import React from 'react';
 
 import style from './Coding.module.css';
 import MenuItem from './MenuItem/MenuItem';
+import FadeIn from 'react-fade-in';
 
 function Coding() {
 	return (
-		<div className={style.codingContainer}>
-			<div className={style.menuContainer}>
-				<h1 className={style.title}>Projects</h1>
+		<FadeIn>
+			<div className={style.codingContainer}>
+				<div className={style.menuContainer}>
+					<h1 className={style.title}>Projects</h1>
+					<FadeIn>
+						<MenuItem
+							title="kevinchang.com"
+							date="Jul 20, 2020"
+							lang="ReactJS"
+							githubLink="https://github.com/Kevinjchang98/kevinchang-website"
+						/>
 
-				<MenuItem
-					title="kevinchang.com"
-					date="Jul 20, 2020"
-					lang="ReactJS"
-					githubLink="https://github.com/Kevinjchang98/kevinchang-website"
-				/>
+						<MenuItem
+							title="LoL Stats"
+							date="Jun 8, 2020"
+							lang="R"
+							githubLink="https://github.com/Kevinjchang98/INFO201-League-Stats"
+						/>
 
-				<MenuItem
-					title="LoL Stats"
-					date="Jun 8, 2020"
-					lang="R"
-					githubLink="https://github.com/Kevinjchang98/INFO201-League-Stats"
-				/>
-
-				<MenuItem
-					title="Various Physics Labs"
-					date="Jun 8, 2020"
-					lang="Matlab, LaTeX"
-					githubLink="https://github.com/Kevinjchang98/PHYS432"
-				/>
+						<MenuItem
+							title="Various Physics Labs"
+							date="Jun 8, 2020"
+							lang="Matlab, LaTeX"
+							githubLink="https://github.com/Kevinjchang98/PHYS432"
+						/>
+					</FadeIn>
+				</div>
 			</div>
-		</div>
+		</FadeIn>
 	);
 }
 
--- src/Components/Photo/Photo.jsx
+++ src/Components/Photo/Photo.jsx
@@ -7,31 +7,34 @@ import erikaImage from './img/erika.png';
 import nanjingImage from './img/nanjing.png';
 import cherryImage from './img/cherryblossom.png';
 import MenuRight from './MenuRight/MenuRight';
+import FadeIn from 'react-fade-in';
 
 function Photo() {
 	return (
-		<div className={style.photoContainer}>
-			{/* Consider putting link inside component so sides aren't clickable maybe */}
-			<MenuLeft
-				image={erikaImage} //
-				title="Erika"
-				link="/photo/erika"
-				isEnglish={true}
-			/>
-			<MenuRight
-				image={cherryImage} //
-				title="桜祭り"
-				link="/photo/cherryblossom"
-				isEnglish={false}
-				imageRight={true}
-			/>
-			<MenuLeft
-				image={nanjingImage} //
-				title="南京"
-				link="/photo/nanjing"
-				isEnglish={false}
-			/>
-		</div>
+		<FadeIn>
+			<div className={style.photoContainer}>
+				{/* Consider putting link inside component so sides aren't clickable maybe */}
+				<MenuLeft
+					image={erikaImage} //
+					title="Erika"
+					link="/photo/erika"
+					isEnglish={true}
+				/>
+				<MenuRight
+					image={cherryImage} //
+					title="桜祭り"
+					link="/photo/cherryblossom"
+					isEnglish={false}
+					imageRight={true}
+				/>
+				<MenuLeft
+					image={nanjingImage} //
+					title="南京"
+					link="/photo/nanjing"
+					isEnglish={false}
+				/>
+			</div>
+		</FadeIn>
 	);
 }
 
